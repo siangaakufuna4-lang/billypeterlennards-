71057f3101f6160d50ed1f9d60af1cb066512ecb
siangaakufuna4-lang <siangaakufuna4@gmail.com>
2026-08-26 12:53:00 +0200

    Add files via upload

---
+++ TASK2.py
@@ -0,0 +1,8 @@
+
+name = input ("what is your name?" )
+print(f"Hi {name}, nice meeting you today ! ")
+feeling = input("How are you feeling ? ")
+if feeling.lower()=="good":
+    print("wow! what excites you today")
+elif feeling.lower() =="bad":
+    print("sorry to hear that,what could be the problem")
